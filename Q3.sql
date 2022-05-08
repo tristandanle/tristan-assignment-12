77cb744d48b399fc5c563e8c8e3ca0f60c11700c
tristandanle <letristandan@gmail.com>
2022-05-08 09:50:07 -0400

    Update Q3.sql

--- Q3.sql
+++ Q3.sql
@@ -45,3 +45,48 @@ join pizzas_orders po on po.order_id = o.order_id
 join pizzas p on p.pizza_id = po.pizza_id
  where o.order_id = 3
 group by(p.pizza_type);
+
+-- Order #4
+select 
+o.order_id as "Order #", 
+c.customer_name as "Name",
+c.telephone as "Telephone",
+o.order_date as "Date",
+p.pizza_type as "Type" ,count(p.pizza_type) as "Quantity"
+from customers c 
+join customers_orders co on co.customer_id = c.customer_id
+join orders o on co.order_id = o.order_id
+join pizzas_orders po on po.order_id = o.order_id
+join pizzas p on p.pizza_id = po.pizza_id
+ where o.order_id = 4
+group by(p.pizza_type);
+
+-- Order #5
+select 
+o.order_id as "Order #", 
+c.customer_name as "Name",
+c.telephone as "Telephone",
+o.order_date as "Date",
+p.pizza_type as "Type" ,count(p.pizza_type) as "Quantity"
+from customers c 
+join customers_orders co on co.customer_id = c.customer_id
+join orders o on co.order_id = o.order_id
+join pizzas_orders po on po.order_id = o.order_id
+join pizzas p on p.pizza_id = po.pizza_id
+ where o.order_id = 5
+group by(p.pizza_type);
+
+-- Order #6
+select 
+o.order_id as "Order #", 
+c.customer_name as "Name",
+c.telephone as "Telephone",
+o.order_date as "Date",
+p.pizza_type as "Type" ,count(p.pizza_type) as "Quantity"
+from customers c 
+join customers_orders co on co.customer_id = c.customer_id
+join orders o on co.order_id = o.order_id
+join pizzas_orders po on po.order_id = o.order_id
+join pizzas p on p.pizza_id = po.pizza_id
+ where o.order_id = 6
+group by(p.pizza_type);
